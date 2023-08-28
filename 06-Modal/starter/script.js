e3b9e030063bc7a7a8f57689f77bb37c5bc14cac
Michael Thompson <michael.a.thompson@capgemini.com>
2023-08-28 15:55:57 -0400

    Modal lesson updates.

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -4,12 +4,14 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btn = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
-console.log(btnsOpenModal);
 
 for (let i = 0; i < btnsOpenModal.length; i++) {
-  console.log(
-    btnsOpenModal[i].addEventListener('click', function () {
-      alert('hey');
-    })
-  );
+  btnsOpenModal[i].addEventListener('click', function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
 }
+btn.addEventListener('click', function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+});
