24c429833a602dc7349958c9913b66dda7b4c2c0
Michael Thompson <mikethompson100@hotmail.com>
2023-08-29 17:31:38 -0400

    Continuing with Rolling the Dice lession.

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -2,3 +2,28 @@
 
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+let currentScore = 0;
+
+btnRoll.addEventListener('click', function () {
+  // 1. Generating a random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  // 2. Display dice
+  diceEl.src = `dice-${dice}.png`;
+  diceEl.classList.remove('hidden');
+  // 3. Check for rolled 1: if true switch to next player
+  if (dice !== 1) {
+    // Add dice to current score
+    currentScore += dice;
+  } else {
+    // Switch to next player
+  }
+});
