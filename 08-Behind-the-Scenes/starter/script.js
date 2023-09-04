5c59b681cefae83bb0ce22f84460f5b990d90b58
Michael Thompson <mikethompson100@hotmail.com>
2023-09-04 12:59:31 -0400

    Scoping practice lesson.

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,2 +1,25 @@
 'use strict';
 /// Mostly lecture lesson
+
+function calcAge(birthyear) {
+    const age = 2037 - birthyear;
+    function printAge() {
+        const output = `You are the ${age}, born in ${birthyear}`;
+        console.log(output);
+
+        if(birthyear >= 1969 && birthyear <= 1996)
+        {
+            const str = `Oh, and you are a millenial, ${firstName}`;
+            console.log(str)
+            function add(a,b) {
+                return a + b;
+            }
+        }
+        console.log(add(2, 5));
+    }
+    printAge();
+    return age;
+}
+
+const firstName = 'Mike';
+console.log(calcAge(1970));
