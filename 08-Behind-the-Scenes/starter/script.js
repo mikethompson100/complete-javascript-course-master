432e4eb91325a0922af6a46da5a345d659558a07
Michael Thompson <mikethompson100@hotmail.com>
2023-09-04 14:24:28 -0400

    Finished hoisting lesson.

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,21 +1,25 @@
 'use strict';
 /// Mostly lecture lesson
-
+/* 
 function calcAge(birthyear) {
     const age = 2037 - birthyear;
     function printAge() {
-        const output = `You are the ${age}, born in ${birthyear}`;
+        let output = `You are the ${age}, born in ${birthyear}`;
         console.log(output);
 
         if(birthyear >= 1969 && birthyear <= 1996)
         {
+            const firstName = "Michael";
             const str = `Oh, and you are a millenial, ${firstName}`;
             console.log(str)
             function add(a,b) {
                 return a + b;
             }
+            output = 'NEW OUTPUT';
+
         }
-        console.log(add(2, 5));
+        //console.log(add(2, 5));
+        console.log(output);
     }
     printAge();
     return age;
@@ -23,3 +27,54 @@ function calcAge(birthyear) {
 
 const firstName = 'Mike';
 console.log(calcAge(1970));
+ */
+
+/* console.log(me);
+console.log(job);
+console.log(year);
+
+var me = 'Michael Thompson';
+let job = 'Full Stack Web Developer';
+const year = 2024; */
+
+// functions
+
+/* console.log(addDecl(2,3));
+console.log(addExpr(2,3));
+console.log(addArrow(2,3));
+
+
+function addDecl(a,b) {
+    return a + b;
+}
+
+const addExpr = function(a,b) {
+    return a + b;
+}
+
+const addArrow = (a,b) => a + b; */
+console.log(numProducts);
+if(!numProducts) deleteShoppingCart();
+
+console.log(numProducts);
+var numProducts = 10;
+
+console.log(numProducts);
+function deleteShoppingCart() {
+    console.log('All products deleted!');
+}
+
+var x = 1;
+let y = 2;
+const z = 3;
+
+
+
+
+
+
+
+
+
+
+
