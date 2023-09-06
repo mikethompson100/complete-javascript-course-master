b8286e92207b1ef2038ac725a5030519b721e38b
Michael Thompson <mikethompson100@hotmail.com>
2023-09-05 20:03:15 -0400

    Finished lession #97 on this keyword.

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -68,21 +68,38 @@ var x = 1;
 let y = 2;
 const z = 3; */
 
-console.log(this);
-
+//console.log(this);
+/* 
 const calcAge = function(birthyear) {
     console.log(2037 - birthyear);
-    console.log(this);
+    console.log("1" + this);
 }
 calcAge(1991);
 
 const calcAgeArrow = birthyear => {
 console.log(2037 - birthyear);
-console.log(this);
+console.log("2" + this);
 }
-calcAgeArrow(1980);
+calcAgeArrow(1980); */
 
+const jonas = {
+    year: 1991,
+    calcAge: function () {
+        console.log(2037 - this.year);
+    }
+}
+jonas.calcAge();
 
+const matilda = {
+    year: 2017
+}
+
+matilda.calcAge = jonas.calcAge;
+matilda.calcAge(this);
+
+const f = jonas.calcAge;
+
+f();
 
 
 
