21117911008a58076c54a7dc9487a1378f0b4047
Michael Thompson <mikethompson100@hotmail.com>
2023-09-08 15:41:21 -0400

    Lesson 107. Short circuit part 2 &&.

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -42,13 +42,27 @@ const restaurant = {
 
   }
 };
-
+console.log('==== OR ====');
 restaurant.numGuests = 23;
 const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
 console.log(guests1)
 
 const guests2 = restaurant.numGuests || 10;
 console.log(guests2);
+console.log('==== OR ====');
+
+
+console.log('==== AND ====');
+
+
+
+
+
+
+
+
+
+
 
 
 /* console.log(restaurant.orderPizza('Mushrooms', 'Pepperoni', 'Meatballs'))
