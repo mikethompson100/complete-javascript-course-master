7a47e0db81fa1e07fddc28bb3161381741277d69
Michael Thompson <mikethompson100@hotmail.com>
2023-09-11 08:36:31 -0400

    Lesson 113. Optional chaining operator. Part 1.

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -5,24 +5,25 @@ const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
 
-const weekdays = ['mon','tue','wed','thu','fri','sat','sun'];
-const openingHours = {
-  [weekdays[3]]: {
-    open: 12,
-    close: 22,
-  },
-  [weekdays[4]]: {
-    open: 11,
-    close: 23,
-  },
-  [`day#${2 + 4}`]: {
-    open: 0, // Open 24 hours
-    close: 24,
-  },
-};
-console.log(openingHours);
-
-
+  const weekdays = ['mon','tue','wed','thu','fri','sat','sun'];
+  const openingHours = {
+    'mon': {
+      open: 12,
+      close: 22,
+    },
+    [weekdays[3]]: {
+      open: 12,
+      close: 22,
+    },
+    [weekdays[4]]: {
+      open: 11,
+      close: 23,
+    },
+    [`day#${2 + 4}`]: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  };
 
 // Data needed for first part of the section
 const restaurant = {
@@ -48,7 +49,22 @@ const restaurant = {
 
   }
 };
-console.log(restaurant);
+//console.log(restaurant);
+
+
+//console.log(openingHours);
+
+if (restaurant.openingHours && restaurant.openingHours.fri) {
+  //console.log(restaurant.openingHours.fri.open + ".");
+}
+
+/// With optional chaining
+
+console.log(restaurant.openingHours.mon?.open);
+console.log(restaurant.openingHours?.mon?.open);
+
+
+
 
 const rest1 = {
   name: 'Capri',
@@ -176,7 +192,7 @@ const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
 //for (const item of menu) console.log(item);
 
 for (const item of menu.entries()) {
-  console.log(`${item[0] + 1}: ${item[1]}`)
+  //console.log(`${item[0] + 1}: ${item[1]}`)
 }
 
 /* 
