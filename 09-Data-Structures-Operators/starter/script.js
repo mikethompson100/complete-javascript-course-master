58104961aa0bbbf8335d9a7aacd83b3cafc0201c
Michael Thompson <mikethompson100@hotmail.com>
2023-09-13 07:07:35 -0400

    Lesson 115. Coding challenge. Part 1.

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -70,7 +70,7 @@ for (const day of Object.keys(openingHours)) {
   //console.log(day);
 }
 
-const values = Object.values(openingHours);
+/* const values = Object.values(openingHours);
 //console.log(values);
 
 const entries = Object.entries(openingHours);
@@ -79,7 +79,7 @@ console.log(entries);
 for (const [key, {open, close}] of entries) {
   console.log(`On ${key} we open at ${open} and close at ${close}`);
 }
-
+ */
 
 
 
@@ -277,4 +277,77 @@ const [i, , [j,k]] = nested;
 
 const [p,q,r] = [8,9]
 console.log(p,q,r); */
+
+
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+/* for (const [i, player] of game.scored.entries())
+  console.log(`Goal ${i + 1}: ${player}`);
+ */
+for (let [key, name] of game.scored.entries()) {
+  //console.log(`Goal: ${key+1} ${name}`);
+}
+
+/* const odds = Object.values(game.odds);
+let average = 0;
+for (const odd of odds) average += odd;
+average /= odds.length; */
+//console.log(average); 
+
+const odds = Object.entries(game.odds);
+for (const [odd] of odds) {
+  //string = (odd != 'x') ? (`victory ${game[odd]}: ${game.odds[odd]}`) : (`draw ${game.odds[odd]}`);
+  //console.log(`Odds of ${string}`)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
