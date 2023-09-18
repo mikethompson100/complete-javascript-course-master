73b72f865a6b8b1153e4e7b2ae075140f6a422c5
Michael Thompson <mikethompson100@hotmail.com>
2023-09-18 07:02:55 -0400

    Lesson 118. Part 2.

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -450,11 +450,22 @@ const question = new Map([
   [true, 'Correct!'],
   [false, 'Try again!']
 ]);
-console.log(question);
+console.log(question); 
 
+console.log(Object.entries(openingHours));
 
+const hoursMap = new Map(Object.entries(openingHours));
+console.log(hoursMap);
 
+console.log(question.get('question'));
+for (const [key, value] of question) {
+    if (typeof key === 'number') {
+      console.log(`Answer ${key}: ${value}`);
+    }
+}
 
+const answer = Number(prompt("Your answer?"));
+console.log(answer);
 
-
-
+console.log(question.get((question.get('correct') === answer)));
+ 
