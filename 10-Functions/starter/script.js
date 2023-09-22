1e9f04bcef8b9a1a6d305a7504cbe163e7b00121
Michael Thompson <mikethompson100@hotmail.com>
2023-09-22 08:33:28 -0400

    Lesson 128. Default Parameters lesson.

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,38 @@
 'use strict';
+
+const bookings = [];
+
+const createBooking = function(flightNum = 123, numPassengers = 1, price = 0) {
+    const booking = {
+        flightNum,
+        numPassengers,
+        price
+    }
+    console.log(booking);
+    bookings.push(booking);
+}
+
+createBooking('test', undefined , 5);
+
+console.log(bookings);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
