3e9bb9df20ecc3d9fab374aaef80e219cbc45852
Michael Thompson <mikethompson100@hotmail.com>
2023-09-22 06:54:40 -0400

    Lesson 125. Completed Section 9 Data Structures, Modern Operators and Strings.

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Data needed for a later exercise
-const flights =
+//const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
 const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
@@ -634,7 +634,7 @@ planesInLine(5);
 
 /// coding challenge #4
 
-document.body.append(document.createElement('textarea'));
+/* document.body.append(document.createElement('textarea'));
 document.body.append(document.createElement('button'));
 
 document.querySelector('textarea').innerHTML = 'Paste text here';
@@ -647,7 +647,7 @@ document.querySelector('button').addEventListener("click", function() {
     console.log(`${output.padEnd(20)} ${'✅'.repeat(index+1)}`);
   }
 });
-
+ */
 
 /* function runConversion(data) {
   const words = data.split('\n');
@@ -672,9 +672,57 @@ document.querySelector('button').addEventListener("click", function() {
  */
 
 
+///////////////////////////////////////
+// String Methods Practice
+
+const flights = '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+console.log(flights);
+// 🔴 Delayed Departure from FAO to TXL (11h25)
+//              Arrival from BRU to FAO (11h45)
+//   🔴 Delayed Arrival from HEL to FAO (12h05)
+//            Departure from FAO to LIS (12h30)
+
+// _Delayed_Departure;fao93766109;txl2133758440;11:25
+
+/* const array = flights.split('+');
+console.log(array);
+
+for (const a of array) {
+const [status, arrive, depart, time] = a.split(';')
+console.log(status, arrive, depart, time);
+
+} */
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const getCode = str => str.slice(0, 3).toUpperCase();
+
+for (const flight of flights.split('+')) {
+  const [type, from, to, time] = flight.split(';');
+  const output = `${type.startsWith('_Delayed') ? '🔴' : ''}${type.replaceAll(
+    '_',
+    ' '
+  )} from ${getCode(from)} to ${getCode(to)} (${time.replace(':', 'h')})`.padStart(50);
+  console.log(output);
+}
+
+ 
 
 
 
