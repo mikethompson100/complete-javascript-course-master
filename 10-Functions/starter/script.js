0e08023f947ffd01bb5d72a77bbae06299bda26f
Michael Thompson <mikethompson100@hotmail.com>
2023-09-22 09:12:26 -0400

    Lesson 129. Finished lesson. Passing arguments, value versus reference.

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/* 
 const bookings = [];
 
 const createBooking = function(flightNum = 123, numPassengers = 1, price = 0) {
@@ -14,11 +14,37 @@ const createBooking = function(flightNum = 123, numPassengers = 1, price = 0) {
 
 createBooking('test', undefined , 5);
 
-console.log(bookings);
+console.log(bookings); */
 
+const flight = 'LH123';
+const jonas = {
+    name: 'Jonas Schmedtmann',
+    passport: 130280
+}
 
+const checkIn = function (flightNum, passenger) {
+    flightNum = 'LH456';
+    passenger.name = 'Mr. ' + passenger.name;
 
+    if(passenger.passport === 130280) {
+        alert("checkin");
+    }
+    else alert("Wrong passport")
+}
 
+checkIn(flight, jonas);
+/* 
+console.log(flight);
+console.log(jonas); */
+
+const newPassport = function(person) {
+    person.passport = Math.trunc(Math.random() * 10000000000);
+
+}
+
+newPassport(jonas);
+checkIn(flight, jonas);
+console.log(jonas);
 
 
 
