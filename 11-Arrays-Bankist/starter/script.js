29eafdb5171d5724251df520fcf57c86f4fcfe89
Michael Thompson <mikethompson100@hotmail.com>
2023-10-04 06:57:57 -0400

    Lessons 151: Started Map and forEach Method example. Part2.

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -81,8 +81,12 @@ const displayMovements = function (movements) {
 displayMovements(account1.movements);
 
 const user = 'Steven Thomas Williams';
-const username = user.toLowerCase().split(' ');
-
+const username = user
+.toLowerCase()
+.split(' ')
+.map(name => name[0])
+  .join('');
+  console.log(username);
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
