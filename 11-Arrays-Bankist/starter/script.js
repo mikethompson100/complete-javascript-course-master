ec6616e3b0078f29de9a634031f9919e9f5fb3ca
Michael Thompson <mikethompson100@hotmail.com>
2023-10-12 15:27:09 -0400

    Lessons 159: Implementing transfers. Bankist App.

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -129,7 +129,12 @@ btnLogin.addEventListener('click', function(e) {
 });
 
 
-
+btnTransfer.addEventListener('click', function(e) {
+  e.preventDefault();
+  const amount = Number(inputTransferAmount.value);
+  const receiverAcc = accounts.find(acc => acc.username === inputTransferTo.value);
+  console.log(amount, receiverAcc);
+})
 
 /* console.log(accounts);
 const movements = account1.movements;
