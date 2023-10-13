871e9ddbd774507a3d65a616bcf790ce49f98f7b
Michael Thompson <mikethompson100@hotmail.com>
2023-10-13 12:48:06 -0400

    Finished lesson 161 on some and every. Starting lesson 162 on Flat and Flapmap methods.

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -364,7 +364,26 @@ console.log(firstWithdrawal);
 console.log(account); */
  
 /// Every 
-console.log(movements.every(mov => mov > 0));
+///console.log(movements.every(mov => mov > 0));
+
+/// Flat
+/* const arr = [[1,2,3], [4,5,6], 7, 8];
+console.log(arr);
+console.log(arr.flat());
+
+const arrDeep = [[[1,2] ,3], [4,5,6], 7, 8];
+console.log(arrDeep);
+console.log(arrDeep.flat(2)); */
+
+console.log(accounts);
+
+const accountMovements = accounts.map(acc => acc.movements).flat();
+
+console.log(accountMovements);
+
+
+
+
 
 
 
