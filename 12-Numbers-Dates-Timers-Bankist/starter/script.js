f3d68215a06a3f9480d6140b0098568dd8cf0914
Michael Thompson <mikethompson100@hotmail.com>
2023-10-21 19:26:40 -0400

    Lesson 171. Continuing lesson on Math and Rounding.

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -297,19 +297,12 @@ console.log(Math.min(5,18,23,11,2));
 
 // Constant
 console.log(Math.PI);
+console.log(Math.PI * Number.parseFloat('10px')** 2);
 
+console.log(Math.trunc(Math.random() * 6) + 1);
+const randomInt = (min, max) => Math.trunc(Math.random() * (max-min) + 1 + min);
 
-
-
-
-
-
-
-
-
-
-
-
+console.log(randomInt(1,21));
 
 
 
