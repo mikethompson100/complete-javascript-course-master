626c0e66d0543fd6d60b2917f99ab7a30ccc2157
Michael Thompson <mikethompson100@hotmail.com>
2023-10-21 19:12:58 -0400

    Lesson 171. Lesson on ksqrt, max, min, PI.

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -285,6 +285,31 @@ console.log("Number.isInteger(23/0)--", Number.isInteger(23/0));
 
 /// Lesson 171 - Math and Rounding
 
+console.log(Math.sqrt(25));
+console.log(225 ** (1/2));
+console.log(8 ** (1/3));
+
+console.log(Math.max(5,18,23,11,2));
+console.log(Math.max(5,18,'23',11,2));
+console.log(Math.max(5,18,'23px',11,2));
+
+console.log(Math.min(5,18,23,11,2));
+
+// Constant
+console.log(Math.PI);
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
