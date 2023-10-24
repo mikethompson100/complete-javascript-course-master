04dbc5e6435105ce33d843c3806f5ea22f82ce9d
Michael Thompson <mikethompson100@hotmail.com>
2023-10-24 11:03:47 -0400

    Lesson 176. Finished lesson.

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -171,8 +171,9 @@ const now = new Date();
 const day = `${now.getDate()}`.padStart(2, 0);
 const month = `${(now.getMonth() + 1)}`.padStart(2, 0);
 const year = now.getFullYear();
-const hour = now.getHours();
-const min = now.getMinutes();
+const hour = `${now.getHours()}`.padStart(2,0);
+const min = `${now.getMinutes()}`.padStart(2,0);
+
 labelDate.textContent = `${month}/${day}/${year}, ${hour}:${min}`;
 
 
