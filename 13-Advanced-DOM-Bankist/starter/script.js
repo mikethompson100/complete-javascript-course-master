3dde03fad0c791fdd976cc5d760eeac905f3e19f
Michael Thompson <mikethompson100@hotmail.com>
2023-11-01 10:53:04 -0400

    Lesson 192. Event delegation lesson. Part 2.

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -1,13 +1,37 @@
 'use strict';
 
-///////////////////////////////////////
-// Modal window
-
+// Button scrolling
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
+const btnScrollTo = document.querySelector('.btn--scroll-to')
+const section1 = document.getElementById('section--1');
+btnScrollTo.addEventListener('click', function(e) {
+  e.preventDefault;
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords); 
+console.log('Current scroll X/Y: ', window.pageXOffset, window.pageYOffset);
+console.log('height/width viewport: ', document.documentElement.clientHeight, document.documentElement.clientWidth);
+});
+
+// Page navigation
+
+document.querySelectorAll('.nav__link').forEach(function(el) {
+  el.addEventListener('click', function(e) {
+    console.log("link");
+  })
+});
+
+
+
+
+
+
+
+///////////////////////////////////////
+// Modal window
 const openModal = function (e) {
   e.preventDefault();
   modal.classList.remove('hidden');
@@ -34,52 +58,61 @@ document.addEventListener('keydown', function (e) {
 });
 
 
-const header = document.querySelector('.header');
+
+
+
+
+
+
+
+////////////////////////////////////////////////////////////////////
+
+/* const header = document.querySelector('.header');
 const allSections = document.querySelectorAll('.section');
 
 
 document.getElementById('section--1');
 const allButtons = document.getElementsByTagName('button');
 console.log(allButtons);
-const allSectionsArray = Array.from(allButtons);
-
-console.log(document.getElementsByClassName('btn'));
+const allSectionsArray = Array.from(allButtons); */
+/* 
+console.log(document.getElementsByClassName('btn')); */
 
 // Creating and inserting elements
-const message = document.createElement('div');
+/* const message = document.createElement('div');
 message.classList.add('cookie-message');
 message.textCOntent = 'We used cookies for improved functionality and analytics';
 message.innerHTML = 'We used cookies for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
 
-header.append(message);
+header.append(message); */
 //header.prepend(message);
 //header.append(message.cloneNode(true));
 
-document.querySelector('.btn--close-cookie').
+/* document.querySelector('.btn--close-cookie').
 addEventListener('click', function() {
   message.remove();
-});
+}); */
 
 // Styles
-message.style.backgroundColor = "#37383d";
+/* message.style.backgroundColor = "#37383d";
 message.style.width = '120%';
 message.style.padding = "10px 0";
 
-console.log(getComputedStyle(message).color);
+console.log(getComputedStyle(message).color); */
 
 // Attributes
-const logo = document.querySelector('.nav__logo');
+/* const logo = document.querySelector('.nav__logo');
 console.log(logo.alt);
-console.log(logo.src);
+console.log(logo.src); */
 
 // data Attributes
-console.log(logo.getAttribute('data-version-number'));
+/* console.log(logo.getAttribute('data-version-number'));
 console.log(logo.dataset);
-console.log(logo.dataset.versionNumber);
+console.log(logo.dataset.versionNumber); */
 
 // classList
 
-logo.classList.add('test');
+//logo.classList.add('test');
 
 // # 188
 /* 
@@ -125,7 +158,7 @@ h1.addEventListener('mouseenter', func); */
 
 /// rgb(255,255,255);
 
-const randomInt = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+/* const randomInt = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
 const randomColor = () => `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
 
 document.querySelector('.nav__link').addEventListener('click', function (e) {
@@ -142,7 +175,7 @@ document.querySelector('.nav').addEventListener('click', function(e) {
   this.style.backgroundColor = randomColor();
   console.log('LINK: ', e.target, e.currentTarget);
 });
-
+ */
 /// #192  Event delegation 
 
 
