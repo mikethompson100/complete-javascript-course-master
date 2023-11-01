424640aaa6f824d3084f7d0ebbb41670e4798584
Michael Thompson <mikethompson100@hotmail.com>
2023-11-01 17:58:08 -0400

    Lesson 192. Event delegation lesson. Part 3

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -18,13 +18,29 @@ console.log('height/width viewport: ', document.documentElement.clientHeight, do
 
 // Page navigation
 
-document.querySelectorAll('.nav__link').forEach(function(el) {
+/* document.querySelectorAll('.nav__link').forEach(function(el) {
   el.addEventListener('click', function(e) {
-    console.log("link");
+    e.preventDefault();
+    const id = this.getAttribute("href");
+    document.querySelector(id).scrollIntoView({behavior: 'smooth'});
   })
+}); */
+
+// 1. Add event listener to common parent elements
+document.querySelector('.nav__links').addEventListener('click', function(e) {
+ // console.log(e.target);
+  e.preventDefault();
+
+  // Matching strategy
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute("href");
+    document.querySelector(id).scrollIntoView({behavior: 'smooth'});
+  }
 });
 
 
+
+// 2. Find out which element origined the event
 
 
 
