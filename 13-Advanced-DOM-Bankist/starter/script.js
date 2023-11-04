e0462916351329bc392a3176383a6867e02b759d
Michael Thompson <mikethompson100@hotmail.com>
2023-11-04 09:15:15 -0400

    Lesson 194. Finished lesson 194.

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -78,12 +78,15 @@ tabsContainer.addEventListener('click', function(e) {
   const clicked = e.target.closest('.operations__tab');
   console.log(clicked);
 
+  // Guard clause
   if (!clicked) return;
+
+  // Remove active classes
   tabs.forEach(t => t.classList.remove('operations__tab--active'));
-  tabsContent.forEach(c=>c.classList.remove(''));
+  tabsContent.forEach(c=>c.classList.remove('operations__content--active'));
   clicked.classList.add('operations__tab--active');
 
-  // Active content 
+  // Activate tab content 
   console.log(clicked.dataset.tab);
 
   document.querySelector(`.operations__content--${clicked.dataset.tab}`)
