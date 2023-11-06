0c72385fffe8d8d24b6b6ea18c4b0d23f0a1d400
Michael Thompson <mikethompson100@hotmail.com>
2023-11-06 16:55:27 -0500

    Lesson 196. Finished. Sticky nav scroll event.

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -145,8 +145,12 @@ nav.addEventListener('mouseout', handleHover.bind(0.5));
 nav.addEventListener('mouseout', handleHover.bind(1));
 
 // Sticky nav
-window.addEventListener('scroll', function(e) {
+const initialCoords = section1.getBoundingClientRect();
+console.log(initialCoords);
 
+window.addEventListener('scroll', function(e) {
+  if(window.scrollY > initialCoords.top) nav.classList.add('sticky')
+  else nav.classList.remove('sticky');
 });
 
 
