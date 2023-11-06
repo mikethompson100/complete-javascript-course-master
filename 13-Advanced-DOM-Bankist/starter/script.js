5ab53fe10197db589ef3b086e16aa56be8084cd2
Michael Thompson <mikethompson100@hotmail.com>
2023-11-06 15:06:40 -0500

    Lesson 195. Finished.

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -121,15 +121,15 @@ nav.addEventListener('mouseout', function(e) {
   });
 }); */
   
-const handleHover = function (e, opacity) {
+const handleHover = function (e) {
   if(e.target.classList.contains('nav__link')) {
   const link = e.target;
   const siblings = link.closest('.nav').querySelectorAll('.nav__link');
   const logo = link.closest('.nav').querySelector('img');
   siblings.forEach(el => {
-        if (el !== link) el.style.opacity = opacity;
+        if (el !== link) el.style.opacity = this;
         });
-        logo.style.opacity = opacity;
+        logo.style.opacity = this;
   };
 };
 
