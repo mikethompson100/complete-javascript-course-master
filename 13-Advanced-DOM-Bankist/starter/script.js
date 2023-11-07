a79ddbd15b6d9e6185783ab1281896eae2b7706b
Michael Thompson <mikethompson100@hotmail.com>
2023-11-07 07:26:31 -0500

    Lesson 197. Finished lesson.

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -172,16 +172,20 @@ const observer = new IntersectionObserver
 observer.observe(section1); */
 
 const header = document.querySelector('.header');
+const navHeight = nav.getBoundingClientRect().height;;
 
 const stickyNav = function(entries) {
   const [entry] = entries;
   console.log(entry);
+  if(!entry.isIntersecting) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
 }
 
 const headerObserver = new IntersectionObserver(
   stickyNav, {
     root: null,
-    threshold: 0
+    threshold: 0,
+    rootMargin: `-${navHeight}px` /// 90 pixels applied outside the target element
   }
 );
 headerObserver.observe(header);
