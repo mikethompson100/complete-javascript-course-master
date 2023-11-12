6890809da2b14dda0592186f27debd0543861c4b
Michael Thompson <mikethompson100@hotmail.com>
2023-11-11 21:17:05 -0500

    Lesson 200. Building a Slider component. Part 1. Commit 3.

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -191,7 +191,7 @@ const headerObserver = new IntersectionObserver(
 headerObserver.observe(header); */
 
 // Reveal sections     
-/* 
+
 const allSections = document.querySelectorAll('.section');
 
 const header = document.querySelector('.header');
@@ -207,8 +207,8 @@ const revealSection = function(entries, observer) {
 const sectionObserver = new IntersectionObserver(revealSection, { root: null, threshold: 0.15 });
   allSections.forEach(function(section) {
     sectionObserver.observe(section); 
-    section.classList.add('section--hidden');
-  }); */
+    //section.classList.add('section--hidden');
+  }); 
 
 /// Lazy loading images
 
@@ -228,9 +228,9 @@ imgTargets.forEach(img => imgObserver.observe(img));
 
 // Building a slider component Part 1
 
-const slides = document.querySelectorAll('slide');
+const slides = document.querySelectorAll('.slide');
 
-
+slides.forEach((s,i) => s.style.transform = `translateX(${100 * i}%)`);
 
 
 
