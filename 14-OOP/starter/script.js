37c729e46eefa70057b10937a2f9a646c91d732b
Michael Thompson <mikethompson100@hotmail.com>
2023-11-14 16:00:21 -0500

    Lesson 208. Constructor function. Part 1.

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,3 +1,16 @@
 'use strict';
 
 // Object Oriented Programming
+
+const Person = function(firstName, birthYear) {
+console.log(this);
+};
+
+new Person('Jonas', 1991);
+
+// 1. New empty object is created
+// 2. function is called, this = newly create {}
+// 3. {} linked to prototype
+// 4. function automatically returns {} from the beginning
+
+
