3f28b13c340f306aed11c1c168f0709366b20aea
Michael Thompson <mikethompson100@hotmail.com>
2023-11-15 08:55:39 -0500

    Lesson 208. Constructor function. Part 3.

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -8,11 +8,28 @@ this.birthYear = birthYear;
 };
 
 const jonas = new Person('Jonas', 1991);
-console.log(jonas);
 
 // 1. New empty object is created
 // 2. function is called, this = newly create {}
 // 3. {} linked to prototype
 // 4. function automatically returns {} from the beginning
+
+const matilda = new Person('Matilda', 2017);
+const jack = new Person('Jack', 1975);
+
+console.log(jonas);
+console.log(matilda);
+console.log(jack);
+
+console.log(jonas instanceof Person);
+
+
+
+
+
+
+
+
+
 
 
