8de212178ba539bbf999fbce8d1b39295589eb6d
Michael Thompson <mikethompson100@hotmail.com>
2023-11-21 16:55:19 -0500

    Lesson 214. Lesson for object getter and setter. Part 1.

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -102,6 +102,33 @@ PersonCL.prototype.greet = function() {
 };
 jessica.greet();
 
+// Object getters and setters
+
+const account = {
+    owner: 'Jonas',
+    movements: [200, 530, 120, 300],
+
+    get latest() {
+        return this.movements.slice(-1).pop();
+    },
+    set latest(mov) {
+        this.movements.push(mov);
+    }
+};
+
+console.log(account.latest);
+
+account.latest = 50;
+console.log(account.movements);
+
+
+
+
+
+
+
+
+
 
 
 
