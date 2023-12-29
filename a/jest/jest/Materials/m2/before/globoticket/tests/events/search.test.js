aa45bff9dc4258d509ec1080af38574758499bf4
Michael Thompson <mikethompson100@hotmail.com>
2023-12-29 12:47:39 -0500

    Unit testing with Jest. Part 7.

---
+++ a/jest/jest/Materials/m2/before/globoticket/tests/events/search.test.js
@@ -0,0 +1,19 @@
+describe('getEvents', () => {
+    Test("Returns events with ticket prices less than 30", () => {
+        const searchPredicateMock = jest.fn(e => e.ticketPrice < 30);
+
+        const expectedEvent1 = new Event(1, "Pop goes Punk!", 20.00, 1000, 1000);
+        const expectedEvent2 = new Event(4, "Party like it's 1999!", 10.00, 1000, 1000);
+        const expectedEvent3 = new Event(7, "Raven says Nevermore", 29.99, 1000, 1000);
+        
+        const events = [
+            expectedEvent1,
+            new Event(2, "The Music of the Elders", 70.00, 1000, 1000),
+            new Event(3, "Glastonbury 2025", 250.00, 1000, 1000),
+            expectedEvent2,
+            new Event(5, "Radio Tunes Extravaganza", 50.00, 1000, 1000),
+            new Event(6, "Jump Around", 30.01, 1000, 1000),
+            expectedEvent3
+        ]
+    });
+});
