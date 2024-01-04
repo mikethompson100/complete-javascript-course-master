77f9702bbb0d414409e635ef8490fff37111f81c
Michael Thompson <mikethompson100@hotmail.com>
2024-01-04 14:32:30 -0500

    Lesson 247. Async JS, AJAX and APIs.

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -4,12 +4,18 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
-const p = document.querySelector('.p');
+/* const p = document.querySelector('.p');
 setTimeout(function() {
     p.textContent = 'My name is Jonas!';
 }, 5000);
-p.style.color = 'red';
+p.style.color = 'red'; */
 
+const img = document.querySelector('.dog');
+img.src = 'dog.jpg';
+img.addEventListener('load', function() {
+    img.classList.add('fadeIn');
+});
+p.style.width = '300px';
 
 
 
