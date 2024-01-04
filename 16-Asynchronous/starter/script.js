e3d78c9ea80d6991a4d826434c2d36e3cc888ee6
Michael Thompson <mikethompson100@hotmail.com>
2024-01-04 13:39:54 -0500

    Lesson 244. Async. Part 1

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -4,3 +4,23 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+const p = document.querySelector('.p');
+setTimeout(function() {
+    p.textContent = 'My name is Jonas!';
+}, 5000);
+p.style.color = 'red';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
