a673754c28f48192f7f15a874158351b4cd11ab2
Michael Thompson <mikethompson100@hotmail.com>
2024-01-05 10:41:21 -0500

    Lesson 247. Async JS, AJAX and APIs. Part 4.

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -9,12 +9,21 @@ request.open('GET', "https://restcountries.com/v2/name/portugal")
 request.send();
 
 request.addEventListener('load', function() {
-    console.log(this.responseText);
     const [data] = JSON.parse(this.responseText);
     console.log(data);
-})
+});
 
-
+const html = `
+<article class="country">
+<img class="country__img" src="" />
+<div class="country__data">
+  <h3 class="country__name">COUNTRY</h3>
+  <h4 class="country__region">REGION</h4>
+  <p class="country__row"><span>👫</span>POP people</p>
+  <p class="country__row"><span>🗣️</span>LANG</p>
+  <p class="country__row"><span>💰</span>CUR</p>
+</div>
+</article>`;
 
 
 
