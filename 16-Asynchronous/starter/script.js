8fb89ac04980d76c05d1add5a1c6977578059b65
Michael Thompson <mikethompson100@hotmail.com>
2024-02-09 07:37:26 -0500

    Lesson 260. Async. Promise building. Part 8.

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -263,7 +263,7 @@ console.log(lotteryPromise); */
   });
  */
 
-const lotteryPromise = new Promise(function(resolve, reject) {
+/* const lotteryPromise = new Promise(function(resolve, reject) {
     console.log("Lottery draw is happening.");
     setTimeout(function() {
         if(Math.random() >= 0.5) {
@@ -276,4 +276,40 @@ const lotteryPromise = new Promise(function(resolve, reject) {
 });
 
 lotteryPromise.then(res=>console.log(res)).catch(err=>console.error(err));
+ */
 
+/* const fetchPromise = fetch(
+    "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.jsonn",
+  ).then((response) => {
+    console.log("!response= ", response);
+      if (!response.ok) {
+        throw new Error(`HTTP error: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data[0].name);
+    })
+    .catch((error) => {
+      console.error(`Could not get products: ${error}`);
+    }); */
+
+    const fetchPromise1 = fetch(
+        "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json",
+      );
+      const fetchPromise2 = fetch(
+        "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/not-found",
+      );
+      const fetchPromise3 = fetch(
+        "https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json",
+      );
+      
+      Promise.all([fetchPromise1, fetchPromise2, fetchPromise3])
+        .then((responses) => {
+          for (const response of responses) {
+            console.log(`${response.url}: ${response.status}`);
+          }
+        })
+        .catch((error) => {
+          console.error(`Failed to fetch: ${error}`);
+        });
